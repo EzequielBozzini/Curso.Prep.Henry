1bb8cf2513bcc140466f9bb9d05b2fdb7bea8da9
EzequielBozzini <ezebozzini02@gmail.com>
2021-10-07 19:50:26 -0300

    finish homework 02

---
+++ 01-Git/homework/Homework 02/Untitled-1.js
@@ -0,0 +1,6 @@
+
+Variables: "son como cajas de informacion donde uno le pone un valor que despues puede usar y reutilizar" 
+Strings: "son el tipo de variable para escribir letras, caracteres, que van si o si entre comillas"
+Funciones: "sirven para agrupar codigo y reusarlo. A estos se le agregan argumentos que van entre parentesis, los cuales son como variables que van adentro de la respectiva funcion. Return, es cuando vos aseguras que la funcion una vez se ejecuta, devuelva algo"
+Declaraciones: "El comando if agrega la logica a una funcion, mas bien una condicion, permitiendo elegir entre si o no, verdadero o falso, o sea entre 2 opciones/posibilidades" 
+booleanos: "Son los que definen si se cumple o no la condicion dada en la funcion, asignando un valor a true y en caso contrario sera false"
